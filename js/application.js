af1032500cd1826e38288bbaed0efd69ee4a9e56
James Muranga <jmured@gmail.com>
2013-02-05 13:33:35 +0300

    MJ/Prasanna : Added mustache.js

--- js/application.js
+++ js/application.js
@@ -1,14 +1,41 @@
-$(document).ready(function(){
- $('#register_btn').click(function(){
-     var mail_to ="mailto:prasann@thoughtworks.com";
-     var subject ="NSSF Registration";
-     var body="";
-     $.each($('#register_form').serializeArray(),function(i,elem){
-        body += elem["name"] +": ";
-        body += elem["value"] + "%0D";
-     });
-     var href=mail_to+"?subject="+subject+"&body="+body;
-     $('#register_btn').attr('href',href);
-     return true;
- });
+$(document).ready(function () {
+    $('#register_btn').click(function () {
+        var mail_to = "mailto:prasann@thoughtworks.com";
+        var subject = "NSSF Registration";
+        var body = "";
+        $.each($('#register_form').serializeArray(), function (i, elem) {
+            body += elem["name"] + ": ";
+            body += elem["value"] + "%0D";
+        });
+        var href = mail_to + "?subject=" + subject + "&body=" + body;
+        $('#register_btn').attr('href', href);
+        return true;
+    });
+
+    loadPage("login.html");
+    
+    
 });
+
+function loadPage(name) {
+    getTemplate(name);
+}
+function getTemplate(name) {
+    var template = $(name).html();
+    var html = '';
+    $.get(name, function (template) {
+        html = Mustache.to_html(template, []);
+        $('#place_holder').html(html);
+        attachListeners();
+    });
+}
+ function attachListeners(){
+ $('#register_link').on('click',function () {
+        
+        loadPage("register.html");
+    });
+
+    $('#home').on('click',function () {
+        loadPage("login.html");
+    });
+ }
