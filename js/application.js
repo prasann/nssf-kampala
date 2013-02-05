6066d3065a35442f7223722f22af5bb1c9c82191
Prasanna Venkatesan <pvenkat4ever@gmail.com>
2013-02-05 15:59:36 +0300

    Prasanna/MJ Added Sammy

--- js/application.js
+++ js/application.js
@@ -1,6 +1,38 @@
 $(document).ready(function () {
-    loadPage("login.html");
+    var app = Sammy('#main', function () {
+        this.use(Sammy.Mustache);
+        this.get('#/login', function () {
+            loadPage('login.html');
+        });
+        this.get('#/register', function () {
+            loadPage('register.html');
+        });
+        map(this, '#/action', 'action.html');
+        map(this, '#/account', 'account.html');
+        map(this, '#/transactions', 'transactions.html');
+        this.get('#/register_btn', function () {
+            var mail_to = "mailto:prasann@thoughtworks.com";
+            var subject = "NSSF Registration";
+            var body = "";
+            $.each($('#register_form').serializeArray(), function (i, elem) {
+                body += elem["name"] + ": ";
+                body += elem["value"] + "%0D";
+            });
+            var href = mail_to + "?subject=" + subject + "&body=" + body;
+            $('#register_btn').attr('href', href);
+            return true;
+        });
+    });
+    $(function () {
+        app.run('#/login');
+    });
 });
+
+function map(_this, route, template) {
+    _this.get(route, function () {
+        loadPage(template);
+    });
+};
 
 function loadPage(name) {
     getTemplate(name);
@@ -12,39 +44,5 @@ function getTemplate(name) {
     $.get("../templates/" + name, function (template) {
         html = Mustache.to_html(template, []);
         $('#place_holder').html(html);
-        attachListeners();
-    });
-}
-
-function attachListeners() {
-    $('#register_link').on('click', function () {
-        loadPage("register.html");
-    });
-
-    $('#home').on('click', function () {
-        loadPage("login.html");
-    });
-
-    $('#register_btn').click(function () {
-        var mail_to = "mailto:prasann@thoughtworks.com";
-        var subject = "NSSF Registration";
-        var body = "";
-        $.each($('#register_form').serializeArray(), function (i, elem) {
-            body += elem["name"] + ": ";
-            body += elem["value"] + "%0D";
-        });
-        var href = mail_to + "?subject=" + subject + "&body=" + body;
-        $('#register_btn').attr('href', href);
-        return true;
-    });
-    $("#login_btn").on('click', function () {
-        loadPage("action.html");
-
-    });
-    $("#view_balance_btn").on('click', function () {
-        loadPage("account.html");
-    });
-    $('#view_transaction_history').on("click", function () {
-        loadPage("transactions.html");
     });
 }
