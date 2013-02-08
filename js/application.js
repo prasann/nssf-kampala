a51ec2ebb996c642788603805b2811ed24ba8d7c
James Muranga <jmured@gmail.com>
2013-02-08 11:37:59 +0300

    MJ: wired up  balance page

--- js/application.js
+++ js/application.js
@@ -1,41 +1,40 @@
-$(document).ready(function () {
+$(document).ready(function() {
 
     supports_html5_storage();
-    var app = Sammy('body', function () {
-        this.get('#/login', function () {
+    var app = Sammy('body', function() {
+        this.get('#/login', function() {
             loadPage('login.html');
         });
-        this.get('#/register', function () {
+        this.get('#/register', function() {
             loadPage('register.html');
         });
 
-        this.get('#/action', function () {
+        this.get('#/action', function() {
             loadPage('action.html');
         });
-        this.get('#/account', function () {
-            if (supports_html5_storage()) {
-                user_data = localStorage.getItem("user_data");
-                getTemplate_with_data('account.html', user_data);
-            } else {
-                //TODO
-            }
-        });
-        this.get('#/transactions', function () {
-            $.getJSON("http://nssf-spike.herokuapp.com/api/transactions?username=james&password=james", function (data) {
+        this.get('#/account', function() {
+            if(supports_html5_storage()) {
+                user_data = JSON.parse(localStorage.getItem("user_data"));
+                console.log(user_data);
+                getTemplate('account.html', user_data);
+                } else {
+                    doesnot_support_storage();
+                }
+            });
+        this.get('#/transactions', function() {
+            $.getJSON("http://nssf-spike.herokuapp.com/api/transactions", {
+                username: "james",
+                password: "james"
+            }, function(data) {
                 console.log(data);
-
-
-                $.get("../templates/transactions.html", function (template) {
-                    html = Mustache.to_html(template, data);
-                    $('#place_holder').html(html);
-                });
+                getTemplate('transactions.html', user_data);
             });
         });
-        this.get('#/register_btn', function () {
+        this.get('#/register_btn', function() {
             var mail_to = "mailto:prasann@thoughtworks.com";
             var subject = "NSSF Registration";
             var body = "";
-            $.each($('#register_form').serializeArray(), function (i, elem) {
+            $.each($('#register_form').serializeArray(), function(i, elem) {
                 body += elem["name"] + ": ";
                 body += elem["value"] + "%0D";
             });
@@ -43,84 +42,79 @@ $(document).ready(function () {
             $('#register_btn').attr('href', href);
             return true;
         });
-    });
-    $(function () {
+        });
+    $(function() {
         app.run('#/login');
     });
-});
+    });
 
 function map(_this, route, template) {
-    _this.get(route, function () {
+    _this.get(route, function() {
         loadPage(template);
     });
 };
 
 function loadPage(name) {
-    getTemplate(name);
+    getTemplate(name, []);
 }
 
-function getTemplate(name) {
+function getTemplate(name, data) {
 
-    $.get("../templates/" + name, function (template) {
-        try {
-            html = Mustache.render(template, []);
-            $('#place_holder').html(html);
-            bind_login_btn();
-        } catch (ex) {
-            alert(ex);
-        }
-    });
-}
-
-function getTemplate_with_data(name, data) {
-
-    $.get("../templates/" + name, function (template) {
+    $.get("../templates/" + name, function(template) {
         try {
             html = Mustache.render(template, data);
             $('#place_holder').html(html);
             bind_login_btn();
-        } catch (ex) {
+        } catch(ex) {
             alert(ex);
         }
     });
 }
 
+
 function bind_login_btn() {
-    $('#login_btn').on('click', function () {
-        console.log("asd");
+    $('#login_btn').on('click', function() {
         var username = $('input#username').val();
         var password = $('input#password').val();
-        $.getJSON("http://nssf-spike.herokuapp.com/api/authenticate?username=" + username + "&password=" + password, function (data_from_server) {
-            if (data_from_server.result) {
+        $.getJSON("http://nssf-spike.herokuapp.com/api/authenticate", {
+            username: username,
+            password: password
+        }, function(data_from_server) {
+            if(data_from_server.result) {
                 alert('Login successful');
-                getTemplate_with_data("action.html", data_from_server);
-                if (supports_html5_storage()) {
-                    localStorage.setItem("user_data", data_from_server.data);
+                getTemplate("action.html", data_from_server);
+                if(supports_html5_storage()) {
+                    localStorage.setItem("user_data", JSON.stringify(data_from_server.data));
+                    } else {
+                        doesnot_support_storage();
+                    }
                 } else {
-                    //TODO
+                    alert('Login failed try again');
+                    $('input#username').val('');
+                    $('input#password').val('');
                 }
-            } else {
-                alert('Login failed try again');
-                $('input#username').val('');
-                $('input#password').val('');
-            }
+            });
+        return false;
+
         });
-        return false;
-
-    });
-}
-
-function bind_action_btns() {
-    $(document).delegate("div", "click", function () {
-        window.location = $(this).find("a").attr("href");
-    });
-}
-
-
-function supports_html5_storage() {
-    try {
-        return 'localStorage' in window && window['localStorage'] !== null;
-    } catch (e) {
-        return false;
     }
-}
+
+    
+
+    function bind_action_btns() {
+        $(document).delegate("div", "click", function() {
+            window.location = $(this).find("a").attr("href");
+        });
+    }
+
+    function doesnot_support_storage() {
+        alert('doesnot_support_storage');
+    }
+    
+    function supports_html5_storage() {
+        try {
+            return 'localStorage' in window && window['localStorage'] !== null;
+        } catch(e) {
+            return false;
+        }
+    }
